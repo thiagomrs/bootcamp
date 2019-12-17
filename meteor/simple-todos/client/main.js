0e2eecef0e260a460c0b269ae04540c764f2e60a
thimorais <thimorais@dcc.ufmg.br>
2019-12-17 10:56:50 -0300

    Import do task.js no server

--- meteor/simple-todos/client/main.js
+++ meteor/simple-todos/client/main.js
@@ -3,7 +3,6 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
 import App from '../imports/ui/App.js';
-import '../imports/api/tasks.js';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
